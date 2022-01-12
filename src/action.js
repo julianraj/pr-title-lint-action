6389aac30e5dcce8bab4c17446385856da60bf0f
Julian Raj Manandhar <julianraj@users.noreply.github.com>
2022-01-12 17:46:46 -0500

    Update test.yml

--- src/action.js
+++ src/action.js
@@ -1,5 +1,14 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
 async function run() {
-	console.log('hello world');
+	await octokit.issues.createComment({
+		...context.repo,
+		issue_number: pull_request.number,
+		body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
+	  });
 }
 
 run();
